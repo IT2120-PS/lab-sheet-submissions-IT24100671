bb5c379bb6dd8cd1689f1b21887fc9d6531506fc
IT24100671 <IT24100671@my.sliit.lk>
2025-10-07 21:55:58 +0530

    Add files via upload

---
+++ Lab Sheet 09/IT24100671.R
@@ -0,0 +1,10 @@
+setwd("C:\\Users\\ASUS\\Desktop\\IT24100671")
+getwd
+
+#i
+set.seed(123)
+sample_data <- rnorm(25, mean = 45, sd = 2)
+sample_data
+
+#ii
+t.test(sample_data, mu = 46, alternative = "less")
